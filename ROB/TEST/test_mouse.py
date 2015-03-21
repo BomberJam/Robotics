191adad4f55e7eae353a302a575a8a6bfbb601ff
haze-sama <haze-sama@live.fr>
2015-03-21 20:32:06 +0100

    update
    Moving an arbitrary leg to an arbitrary (x, y, z) position

--- ROB/TEST/test_mouse.py
+++ ROB/TEST/test_mouse.py
@@ -1,3 +1,7 @@
+#import pypot.dynamixel
+#from motor_config import all_motors_not_compliant
+#from pypot.robot import from_json
+#from movement import *
 from Tkinter import *
 win=Tk()
 win.geometry("1920x1080")
@@ -6,6 +10,7 @@ x_tmp = 0
 y_tmp = 0
 z_tmp = 0
 debut = 1
+leg = 0
 
 def calcule_difference(x, y, z):
 	global x_tmp, y_tmp, z_tmp, debut
@@ -23,7 +28,7 @@ def calcule_difference(x, y, z):
 	return liste
 
 def xy(event):
-	global group_impair
+	global group_impair, group_pair, leg
 	liste = calcule_difference(event.x/10, event.y/10, event.delta/10)
 	xm = liste[0]
 	ym = liste[1]
@@ -31,13 +36,16 @@ def xy(event):
 	group_impair[0] = group_impair[0] + xm
 	group_impair[3] = group_impair[3] + ym 
 	group_impair[6] = group_impair[6] + zm 
-	xy_data = "x1=%d,  y1=%d,  z1=%d" % (group_impair[0], group_impair[3], group_impair[6])
+	xy_data = "x1=%d,  y1=%d,  z1=%d,  patte=%s" % (group_impair[0], group_impair[3], group_impair[6], leg)
+	set_pos_to_leg (xm, ym, zm, leg)
 	lab=Label(win,text=xy_data)
 	lab.grid(row=0,column=0)
 
-def Control_mouse(impair):
-	global group_impair
+def Control_mouse(impair, pair, l):
+	global group_impair, group_pair, leg, rob
 	group_impair = impair
+	group_pair = pair
+	leg = l
 	win.title("Moving leg's windows")
 	win.bind("<Motion>",xy)
 	mainloop()
@@ -45,5 +53,31 @@ def Control_mouse(impair):
 
 
 if __name__ == '__main__':
+	pair = [120, 120, 120, 70, 0, -70, -70, -70, -70]
 	impair = [120, 120, 120, 0, -70, 70, -70, -70, -70]
-	Control_mouse(impair)
+
+	choix = input ('choix patte: ')
+	if choix ==  1:
+		leg = 'rob.leg1'
+	elif choix == 2:
+		leg = 'rob.leg2'
+	elif choix == 3:
+		leg = 'rob.leg3'
+	elif choix == 4:
+		leg = 'rob.leg4'
+	elif choix == 5:
+		leg = 'rob.leg5'
+	elif choix == 6:
+		leg = 'rob.leg6'
+
+	#rob = from_json('rob.json')
+
+	#all_motors_not_compliant (rob)
+
+	initialize_to_zero(rob, impair, gpair)
+
+	Control_mouse(impair, pair, leg)
+
+	#rob.close()
+
+
